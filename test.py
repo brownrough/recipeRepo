661e89416f714b5fce1d682b86788eee4a5e29c0
brownrough <bamumbwe@gmail.com>
2017-10-19 23:04:52 +0300

    fix: updated tests

--- test.py
+++ test.py
@@ -5,18 +5,28 @@ class FlaskTestCase(unittest.TestCase):
 
     def test_index(self):
         tester = app.test_client(self)
-        response = tester.get('/signIn', content_type='html/css')
+        response = tester.get('/', content_type='html/css')
         self.assertEqual(response.status_code, 200)
 
-    def test_login_page_loads(self):
+    def test_signin_loads(self):
         tester = app.test_client(self)
         response = tester.get('/signIn', content_type='html/css')
         self.assertTrue(b"Login" in response.data)
-    #correct creditials behavious
+    def test_signup_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/signUp', content_type='html/css')
+        self.assertEqual(response.status_code, 200)
 
-    #Incorrect credetials
+    def test_category_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/categories', content_type='html/css')
+        self.assertEqual(response.status_code, 200)
 
-    # #Logout behaviour
+    def test_recipe_loads(self):
+        tester = app.test_client(self)
+        response = tester.get('/recipes', content_type='html/css')
+        self.assertEqual(response.status_code, 200)
+    
 
 
 if __name__ == 'main':
