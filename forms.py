7b9a488aeddaaecf8e877ca992066fde317ef888
brownrough <bamumbwe@gmail.com>
2017-10-19 21:36:45 +0300

    Updated files

--- forms.py
+++
@@ -1,9 +0,0 @@
-from flask.ext.wtf import form
-from wtforms import StringField, SubmitField
-from wtforms.validators import Required
-# getting data from form
-class signIn(Form):
-    email = StringField('what is your name?', validators=[Required])
-    pwd = StringField('what is your password?', validators=[Required])
-    submit = SubmitField('submit')
-
